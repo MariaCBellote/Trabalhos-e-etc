d38a9a1466c7faf2f175d43388af33ba46059bf3
MariaCBellote <159292487+MariaCBellote@users.noreply.github.com>
2024-06-05 10:41:18 -0300

    Add files via upload

--- EX BD-5.sql
+++ EX BD-5.sql
@@ -20,32 +20,42 @@ JOIN EVENTOS EVN ON ATV.EVENTO_ATV= EVN.ID_EVENTO;
 SELECT PAR.NOME,SUM(PA.ATIVIDADE) AS TOTAL FROM PARTICIPANTES PAR
 JOIN PARTICIPANTE_ATIVIDADE PA ON PA.CPF=PAR.CPF
 GROUP BY PAR.NOME;
+
 #6. Mostrar os nomes dos eventos realizados em 2023.
+SELECT NOME_EVENTO FROM EVENTOS
+WHERE YEAR(DATA_EVENTO)='2023';
 
 
 #7. Listar todos os locais, ordenados pelo nome da cidade de A a Z.
-
-
+SELECT LOC.NOME,LOC.CIDADE FROM LOCAIS LOC
+ORDER BY LOC.CIDADE;
 #8. Mostrar os nomes e CPFs dos participantes que não têm empresa cadastrada.
-
-
+SELECT PAR.NOME,PAR.CPF FROM PARTICIPANTES PAR
+WHERE PAR.EMPRESA IS NULL;
 #9. Exibir os nomes dos participantes que estão inscritos em eventos realizados no local com nome 'Centro de Convenções'.
-
-
+SELECT PAR.NOME FROM PARTICIPANTES PAR
+JOIN LOCAIS   ON
+JOIN          ON
+JOIN          ON
+WHERE LOCAIS.NOME LIKE 'Centro de Convenções';
 #10. Para cada entidade organizadora, mostrar o nome da entidade e a média de eventos realizados.
+SELECT ENTIDADE,AVG(EVENTO) AS MEDIA FROM
+(SELECT ENTIDADE_EVENTO AS ENTIDADE
 
 
 #11. Contar quantos participantes têm uma empresa cadastrada.
+SELECT COUNT(*) AS COM_EMPRESA
+WHERE EMPRESA IS NOT NULL;
 
 
 #12. Listar os nomes dos participantes que participaram de mais de 5 atividades.
-
-
+SELECT PAR.NOME FROM PARTICIPANTES PAR
+JOIN PARTICIPANTE_ENTIDADE PA ON PA.CPF=  PAR.CPF AND PA.INSCRICAO=PAR.NUM_INSCRICAO
+HAVING COUNT(PA.ATIVIDADE)>5;
 #13. Mostrar o nome dos participantes e a data da última atividade que eles participaram.
-
+SELECT PAR.NOME,ATV.DATA_ATV
 
 #14. Contar quantas atividades cada evento possui.
 
 
 #15. Para cada participante que participou de um evento, mostrar o CPF, o nome e a porcentagem de eventos em relação ao total de eventos realizados.
-
