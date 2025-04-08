6b888d0082958d12b358f3a0ddc0467ab6eb65ab
MariaCBellote <159292487+MariaCBellote@users.noreply.github.com>
2025-04-08 10:48:55 -0300

    Add files via upload

---
+++ EXcolecaoMapas/src/main/java/com/mycompany/excolecaomapas/EXcolecaoMapas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.excolecaomapas;
+
+/**
+ *
+ * @author alunolab11
+ */
+public class EXcolecaoMapas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
