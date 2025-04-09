9cd292e93c07b3df25b9d433bfb58ba43bb36831
MariaCBellote <159292487+MariaCBellote@users.noreply.github.com>
2025-04-09 10:51:55 -0300

    Add files via upload

--- EXcolecaoMapas/src/main/java/com/mycompany/excolecaomapas/AgendaTelefonica.java
+++ EXcolecaoMapas/src/main/java/com/mycompany/excolecaomapas/AgendaTelefonica.java
@@ -4,10 +4,49 @@
  */
 package com.mycompany.excolecaomapas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author alunolab11
  */
 public class AgendaTelefonica {
+ 
+    private Map<String,Contato> listaContato= new HashMap<>();  
     
+    
+    public void inserir(String n, String num)
+    {
+        Contato contato= new Contato(n,num);
+        listaContato.put(n, contato);
+    }
+    
+    public String buscar(String n)
+    {
+        Contato contato= listaContato.get(n);
+        if (contato!=null)
+        {
+            return contato.getNumero();
+        }
+        else
+        {
+            return "Contato não salvo!";
+        }
+    }
+    
+    public void remover(String n){
+        listaContato.remove(n);
+    }
+    
+    public int tamanho(){
+        return listaContato.size();
+    }
+    
+    public void imprimir(){
+        for(Contato contato: listaContato.values())
+        {
+           System.out.println("Contato: "+ contato);
+        }
+    }
 }
---
+++ EXcolecaoMapas/src/main/java/com/mycompany/excolecaomapas/Contato.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.excolecaomapas;
+
+/**
+ *
+ * @author alunolab11
+ */
+public class Contato {
+    private String numero;
+    private String nome;
+
+    public Contato(String nome, String numero) {
+        this.numero = numero;
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
